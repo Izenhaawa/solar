cc492610ad51bb1bfc9d352e5d19ce7d23981a78
Izen <anshdwivedi44443333@gmail.com>
2025-10-31 19:57:35 +0530

    solar system

---
+++ Assets/Scripts/Scene_Controller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
